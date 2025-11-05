b86787c8505da9d0f231e566565cad6897424e2d
Romana Odehnalova <r.koudelakova@gmail.com>
2025-11-05 12:35:48 +0100

    uprava funkce odstraneni prebytecne casti else if / nahrazeno pouze else

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-
 const cislo1 = prompt ("Zadej první číslo:")
 const cislo2 = prompt ("Zadej druhhé číslo:")
 const cislo3 = prompt ("Zadej třetí číslo:")
 
-const max3 = (cislo1, cislo2, cislo3) =>{
+const max3 = (cislo1, cislo2, cislo3) => {
     if (cislo1 >= cislo2 && cislo1 >= cislo3) {
         return cislo1
     }  else if (cislo2 > cislo1 && cislo2 >= cislo3){
         return cislo2
-    } else if (cislo3 > cislo1 && cislo3 > cislo2){
+    } else {
         return cislo3
     }
 }
