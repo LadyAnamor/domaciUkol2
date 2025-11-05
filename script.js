f0144ea2dc12e505e47f6f76911838b08822853d
Romana Odehnalova <r.koudelakova@gmail.com>
2025-11-05 12:20:58 +0100

    pridana funkce pro zjisteni nejvetsiho cisla ze tri zadanych hodnot

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+const cislo1 = prompt ("Zadej první číslo:")
+const cislo2 = prompt ("Zadej druhhé číslo:")
+const cislo3 = prompt ("Zadej třetí číslo:")
+
 const max3 = (cislo1, cislo2, cislo3) =>{
     if (cislo1 >= cislo2 && cislo1 >= cislo3) {
         return cislo1
@@ -8,4 +13,4 @@ const max3 = (cislo1, cislo2, cislo3) =>{
     }
 }
 
-console.log (max3 (5, 6,9))
+document.body.innerHTML = `<p>Největší ze tří zadaných čísel (${cislo1}, ${cislo2} a ${cislo3}) je: <b> ${max3(cislo1, cislo2, cislo3)} </b></p>`
